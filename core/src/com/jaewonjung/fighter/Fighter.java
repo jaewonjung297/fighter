b4158606755f50af435f64dadf172437a1429855
Jaewon Jung <jaewon.j.1@gmail.com>
2024-02-23 14:45:01 -0500

    refactored Player class and completed collision detection

--- core/src/com/jaewonjung/fighter/Fighter.java
+++ core/src/com/jaewonjung/fighter/Fighter.java
@@ -3,90 +3,71 @@ package com.jaewonjung.fighter;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.Rectangle;
-import org.w3c.dom.css.Rect;
+import com.jaewonjung.fighter.models.Player;
 
 import java.util.ArrayList;
 
 public class Fighter extends ApplicationAdapter {
 	private SpriteBatch batch;
-	private Texture playerImg;
-	private Texture platformImg;
 	private OrthographicCamera camera;
-	private Rectangle player;
-	private float velocityY = 0;
-	private float gravity = (float) -2000;
-	private float jumpVelocity = 800;
-	private ArrayList<Rectangle> platforms = new ArrayList<>();
-
+	private Player player;
+	final private ArrayList<Rectangle> platforms = new ArrayList<>();
+	private ShapeRenderer shape;
 	@Override
 	public void create () {
-		playerImg = new Texture("bucket.png");
-		platformImg = new Texture("bucket.png");
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
+		shape = new ShapeRenderer();
 		Rectangle platform1 = new Rectangle();
+		Rectangle platform2 = new Rectangle();
 		platform1.set(100, 50, 200, 20);
+		platform2.set(400, 200, 200, 20);
 		platforms.add(platform1);
-
-		player = new Rectangle();
-		player.x = 800 / 2 - 64 / 2;
-		player.y = 0;
-		player.width = 64;
-		player.height = 64;
+		platforms.add(platform2);
+		player = new Player();
 	}
 
 	@Override
 	public void render () {
+		//need to detect when unit is not over a platform
 		ScreenUtils.clear(255, 255, 255, 0);
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(playerImg, player.x, player.y);
-		for (Rectangle platform : platforms) {
-			batch.draw(platformImg, platform.x, platform.y, platform.width, platform.height);
-		}
+		player.render(batch);
 		batch.end();
-
-
-		//apply gravity to y velocity
-		velocityY += gravity * Gdx.graphics.getDeltaTime();
-		player.y += velocityY * Gdx.graphics.getDeltaTime();
-
-		//can only jump if model is on the ground
-		if (player.y < 0) {
-			player.y = 0;
-			velocityY = 0;
-		}
-
-		//set boundaries
-		if (player.x < 0) {
-			player.x = 0;
-		}
-		if (player.x > 800 - 64) {
-			player.x = 800 - 64;
+		player.update(platforms);
+		for (Rectangle platform : platforms) {
+			this.shape.begin(ShapeRenderer.ShapeType.Filled);
+			this.shape.setColor(Color.BLUE);
+			this.shape.rect(platform.x, platform.y, platform.width, platform.height);
+			this.shape.end();
 		}
 
 		//making the object move with keyboard inputs
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) player.x -= 200 * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) player.x += 200 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) player.hitbox.x -= 200 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) player.hitbox.x += 200 * Gdx.graphics.getDeltaTime();
 
-		if (Gdx.input.isKeyPressed(Input.Keys.UP) && player.y == 0) {
+		if (Gdx.input.isKeyPressed(Input.Keys.UP) && player.jumpsLeft > 0) {
 			//implement kinematic equations
 			//when the user jumps, up velocity is constant, and is constantly decreasing
-			velocityY =  jumpVelocity;
+			player.onPlatform = false;
+			player.velocityY =  player.jumpVelocity;
+			player.jumpsLeft -= 1;
 		}
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		playerImg.dispose();
+		player.dispose();
 	}
+
 }
